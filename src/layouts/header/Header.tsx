0da850fad69b41dbe5cd8a8800ea64889071d22c
sssghulamrasool <seositesoft7@gmail.com>
2023-03-20 17:28:10 +0500

    header footer and componet add color's variables

--- src/layouts/header/Header.tsx
+++ src/layouts/header/Header.tsx
@@ -2,7 +2,11 @@ import React from 'react'
 
 const Header = () => {
     return (
-        <header>Header</header>
+        <header >
+            <div className='container global-section-width'>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Necessitatibus, praesentium officiis labore nesciunt iure eum consequatur aut ab eius, cum alias expedita repellat reprehenderit cupiditate totam? Delectus enim suscipit labore!
+            </div>
+        </header>
     )
 }
 
