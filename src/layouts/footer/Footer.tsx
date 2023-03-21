9e8b4125e1e8133fdb9b1576de8ef1d2cf9ad55c
seositesoft17@gmail.com <rehmanali1477@gmail.com>
2023-03-21 14:29:07 +0500

    destop footer complete

--- src/layouts/footer/Footer.tsx
+++ src/layouts/footer/Footer.tsx
@@ -1,9 +1,69 @@
-import React from 'react'
+import React from "react";
+import Image from "next/image";
+import logo from "../../assets/images/footer/main-logo.png";
+import Instgram from "../../assets/images/footer/instagram.png";
+import Twitter from "../../assets/images/footer/twitter.png";
 
 const Footer = () => {
-    return (
-        <footer>Footer</footer>
-    )
-}
+  return (
+    <footer className="bg-brandDark2">
+      <div className="global-section-width">
+        {/* <div className="container"> */}
+          <div className="pt-10 px-5 pb-5">
+            <Image src={logo} alt="Gossip GG" />
+            <div className="pt-7 flex flex-row justify-between">
+             <div className="max-w-[70%]">
+             <p className="text-brandLightOpacity100 text-base font-text max-w-[80%]">
+                Gossip.GG brings you the latest scoop from Gaming, Esports,
+                Tech, and a whole lot more. From breaking the latest industry
+                news to in-depth reviews and features, Gossip.GG has you
+                covered.
+              </p>
+             </div>
+              <div className="flex flex-row">
+                <div className="h-10 w-10 rounded-3xl bg-brandDark1 flex justify-center  items-center">
+                  <Image src={Instgram} alt="Instagram Profile" />
+                </div>
+                <div className="h-10 w-10 ml-10 rounded-3xl bg-brandDark1 flex justify-center  items-center">
+                  <Image src={Twitter} alt="Twitter Profile" />
+                </div>
+              </div>
+              <div>
+                <ul className="flex flex-col text-brandLightOpacity100 text-base text-right font-text">
+                  <li>
+                    <a href="#">About us</a>
+                  </li>
+                  <li  className="pt-1">
+                    <a href="#">Contact Us</a>
+                  </li>
+                  <li  className="pt-1">
+                    <a href="#">Careers</a>
+                  </li>
+                </ul>
+              </div>
+            </div>
+            <div>
+            <ul className="flex flex-row text-brandLightOpacity100 pt-10 text-base font-text">
+                  <li>
+                    <a href="#">Disclaimer</a>
+                  </li>
+                  <li className="pl-10">
+                    <a href="#">Privacy Policy</a>
+                  </li>
+                  <li className="pl-10">
+                    <a href="#">Terms of Use</a>
+                  </li>
+                </ul>
+            </div>
+            <div className="flex justify-center items-center pt-7 text-base font-text">
+                <p className="text-brandLightOpacity100">(c)  2023 Gossip.gg - All Rights Reserved </p>
+            </div>
+          </div>
+        </div>
+      {/* </div> */}
+    </footer>
+    
+  );
+};
 
-export default Footer
+export default Footer;
--- tailwind.config.js
+++ tailwind.config.js
@@ -13,14 +13,41 @@ module.exports = {
     typography: (theme) => ({}),
     extend: {
       colors: {
-        'blue': 'yellow',
-        'pink': '#ff49db',
-        'orange': '#ff7849',
-        'green': '#13ce66',
-        'gray-dark': '#273444',
-        'gray': '#8492a6',
-        'gray-light': '#d3dce6',
+        // BRAND LIGHT COLOR'S
+        'brandLightOpacity100': '#e5e5e5',
+        'brandLightOpacity90': 'rgba(229, 229, 229, 0.9)',
+        'brandLightOpacity70': 'rgba(229, 229, 229, 0.7)',
+        'brandLightOpacity50': 'rgba(229, 229, 229, 0.5)',
+        'brandLightOpacity10': 'rgba(229, 229, 229, 0.1)',
+        'brandLightOpacity5': 'rgba(229, 229, 229, 0.05)',
+        // BRAND DARK COLORS
+        'brandDark1': '#2e2a2b',  
+        'brandDark2': '#221e1f',
+        'brandDark3': '#141313',
+        // BRAND COLOR
+        'brand': '#ed4e50',
+        // SEMANTIC COLORS
+        'successIndicator': '#008d52',
+        'errorIndicator': '#d91618'
+
       },
+      screens: {
+        '2xl': {'max': '1535px'},
+        // => @media (max-width: 1535px) { ... }
+  
+        'xl': {'max': '1279px'},
+        // => @media (max-width: 1279px) { ... }
+  
+        'lg': {'max': '1023px'},
+        // => @media (max-width: 1023px) { ... }
+  
+        'md': {'max': '767px'},
+        // => @media (max-width: 767px) { ... }
+  
+        'sm': {'max': '639px'},
+        // => @media (max-width: 639px) { ... }
+      }
+      
     },
   },
   plugins: [require('@tailwindcss/typography')],
