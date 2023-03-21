c8812ca9a1cb64d14da5cf1ec123b6729845a67c
seositesoft17@gmail.com <rehmanali1477@gmail.com>
2023-03-21 15:57:18 +0500

    fix the header problem

--- src/layouts/footer/Footer.tsx
+++ src/layouts/footer/Footer.tsx
@@ -68,7 +68,7 @@ const Footer = () => {
                     <Image src={MobileLogo} alt="Gossip GG" />
                     <p className="pt-3 text-brandLightOpacity100  font-text2 ">
                         Lorem Ipsum is simply dummy text of the printing and typesetting
-                        industry. Lorem Ipsum has been the industry's standard dummy text
+                        industry. Lorem Ipsum has been the industry&#39s standard dummy text
                         ever since the 1500s, when an unknown printer took a galley of type
                         and scrambled it to make a type specimen book.
                     </p>
