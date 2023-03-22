a49f7b1614aaaf43bf1829e35d7e60b2a350380a
seositesoft17@gmail.com <rehmanali1477@gmail.com>
2023-03-22 20:17:42 +0500

    updated code with events slider

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,31 +2,16 @@
 
 import HeroSection from "@/components/herosection";
 import LiveMatch from "@/components/liveMatches";
-<<<<<<< HEAD
-import EsportEvent from "@/components/esportEvents";
-
-
-=======
+import EsportEvent from "@/components/esportEvents"
 import Trending from "@/components/trending/Index";
->>>>>>> 6d3653b282be6f1ecea84761569d182cfa34e00f
 
 export default function Home() {
   return (
-<<<<<<< HEAD
-    <>
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
-    <EsportEvent/>
-    <LiveMatch/>
-    </>
-  )
-=======
     <section className="home-page">
       <Trending />
       <HeroSection />
+      <EsportEvent/>
       <LiveMatch />
     </section>
   );
->>>>>>> 6d3653b282be6f1ecea84761569d182cfa34e00f
 }
