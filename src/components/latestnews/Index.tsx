f632338b1d85c4c3e1213dc39d2a6fedb063d442
sssghulamrasool <seositesoft7@gmail.com>
2023-03-27 17:02:50 +0500

    wroking on latest

--- src/components/latestnews/Index.tsx
+++ src/components/latestnews/Index.tsx
@@ -46,20 +46,22 @@ const LatestNews = () => {
       <div className="global-section-width">
         <SectionSaprator title="Latest News" />
         <div className="2xl:container border border-grayCard rounded dark:border-brandLightOpacity10">
-          <div className="flex flex-wrap pt-5  bg-white rounded dark:bg-brandDark1">
+          <div className="flex flex-wrap pt-5 pb-5  bg-white rounded dark:bg-brandDark1 mw-lg:hidden">
             {items.map((elem, index) => {
               return (
                 <div
                   // p-5
-                  className={`w-1/3  flex  pl-5    ${style.news_items} `}
+                  className={`w-1/3  mw-12:w-1/3 mw-lg:w-full flex     ${
+                    style.news_items
+                  } border-grayCard  dark:border-brandLightOpacity10 ${
+                    index === 2
+                      ? "mw-12:border-r-grayCard mw-12:dark:border-r-brandLightOpacity10"
+                      : ""
+                  }  `}
                   key={index}
                 >
                   <div
-                    className={`${style.news_img_wrap} pr-4 ${
-                      index + 1 === 1 || index + 1 === 2 || index + 1 === 3
-                        ? "pt-0"
-                        : "pt-5"
-                    }   rounded-sm border-b border-b-grayCard pb-5`}
+                    className={`${style.news_img_wrap}  rounded-sm border-grayCard  mw-12:border-b-grayCard  dark:border-brandLightOpacity10 `}
                   >
                     <Image
                       src={elem.picture}
@@ -68,23 +70,17 @@ const LatestNews = () => {
                     />
                   </div>
                   <div
-                    className={`${
-                      style.news_desc
-                    }  pr-5 border-b border-b-grayCard pb-5  ${
-                      index + 1 === 1 || index + 1 === 2 || index + 1 === 3
-                        ? "pt-0"
-                        : "pt-5"
-                    } `}
+                    className={`${style.news_desc} border-grayCard dark:border-brandLightOpacity10`}
                   >
-                    <h4 className="text-base leading-5 text-brandDark2 font-bold montserratfont">
+                    <h4 className="text-base leading-5 text-brandDark2 font-bold montserratfont dark:text-brandLightOpacity100">
                       Bayes Esports appoints York Scheunemann as COO of reputed
                       gaming...
                     </h4>
                     <div className="tags flex py-4 montserratfont">
-                      <div className="text-textColorGray bg-grayCard px-2 py-1.5 rounded mr-2 text-xs leading-4 font-medium hover:bg-grayCardHover cursor-pointer  ">
+                      <div className="text-textColorGray bg-grayCard px-2 py-1.5 rounded mr-2 text-xs leading-4 font-medium hover:bg-grayCardHover cursor-pointer dark:bg-brandLightOpacity10 dark:hover:bg-brandLightOpacity30 dark:text-brandLightOpacity70">
                         News
                       </div>
-                      <div className="text-textColorGray bg-grayCard px-2 py-1.5 rounded text-xs leading-4 font-medium hover:bg-grayCardHover cursor-pointer">
+                      <div className="text-textColorGray bg-grayCard px-2 py-1.5 rounded text-xs leading-4 font-medium hover:bg-grayCardHover cursor-pointer dark:bg-brandLightOpacity10 dark:hover:bg-brandLightOpacity30 dark:text-brandLightOpacity70">
                         Esports
                       </div>
                     </div>
@@ -92,27 +88,163 @@ const LatestNews = () => {
                       <span>
                         <i>3 hrs ago</i>
                       </span>
-                      <span className="border-l border-l-textColorGray pl-2 ml-2 border-textColorGray">
+                      <span
+                        className="border-l border-l-textColorGray pl-2 ml-2 border-textColorGray
+              dark:border-l-brandLightOpacity10
+              "
+                      >
                         5 min read
                       </span>
                     </small>
                   </div>
+                  <div className={`${style.thumnail_img} `}>
+                    <div className="bookmark text-textColorGray hover:text-textColor  dark:text-brandLightOpacity70 dark:hover:text-brandLightOpacity100">
+                      <i className="fa-regular fa-bookmark "></i>
+                    </div>
+                  </div>
+                </div>
+              );
+              // return (
+              //   <div
+              //     // p-5
+              //     className={`w-1/3  mw-12:w-1/2 mw-sm:w-full flex  pl-5    ${style.news_items} `}
+              //     key={index}
+              //   >
+              //     <div
+              //       className={`${style.news_img_wrap} pr-4
+              //       ${
+              //         index + 1 === 4 || index + 1 === 5 || index + 1 === 6
+              //           ? "border-0"
+              //           : "border-b border-b-grayCard"
+              //       }
+              //       ${
+              //         index + 1 === 1 || index + 1 === 2 || index + 1 === 3
+              //           ? "pt-0"
+              //           : "pt-5"
+              //       }   rounded-sm   dark:border-b-brandLightOpacity10  pb-5`}
+              //     >
+              //       <Image
+              //         src={elem.picture}
+              //         alt="Latest new"
+              //         className="rounded-sm"
+              //       />
+              //     </div>
+              //     <div
+              //       className={`${
+              //         style.news_desc
+              //       }  pr-5 mx-14:pr-4  dark:border-b-brandLightOpacity10  pb-5 ${
+              //         index + 1 === 4 || index + 1 === 5 || index + 1 === 6
+              //           ? "border-0"
+              //           : "border-b border-b-grayCard"
+              //       }  ${
+              //         index + 1 === 1 || index + 1 === 2 || index + 1 === 3
+              //           ? "pt-0"
+              //           : "pt-5"
+              //       } `}
+              //     >
+              //       <h4 className="text-base leading-5 text-brandDark2 font-bold montserratfont dark:text-brandLightOpacity100">
+              //         Bayes Esports appoints York Scheunemann as COO of reputed
+              //         gaming...
+              //       </h4>
+              //       <div className="tags flex py-4 montserratfont">
+              //         <div className="text-textColorGray bg-grayCard px-2 py-1.5 rounded mr-2 text-xs leading-4 font-medium hover:bg-grayCardHover cursor-pointer dark:bg-brandLightOpacity10 dark:hover:bg-brandLightOpacity30 dark:text-brandLightOpacity70">
+              //           News
+              //         </div>
+              //         <div className="text-textColorGray bg-grayCard px-2 py-1.5 rounded text-xs leading-4 font-medium hover:bg-grayCardHover cursor-pointer dark:bg-brandLightOpacity10 dark:hover:bg-brandLightOpacity30 dark:text-brandLightOpacity70">
+              //           Esports
+              //         </div>
+              //       </div>
+              //       <small className="text-textColorGray dark:text-brandLightOpacity70 text-xs montserratfont">
+              //         <span>
+              //           <i>3 hrs ago</i>
+              //         </span>
+              //         <span
+              //           className="border-l border-l-textColorGray pl-2 ml-2 border-textColorGray
+              //         dark:border-l-brandLightOpacity10
+              //         "
+              //         >
+              //           5 min read
+              //         </span>
+              //       </small>
+              //     </div>
+              //     <div
+              //       className={`${
+              //         style.thumnail_img
+              //       }  pr-5 mx-14:pr-4  border-r-grayCard dark:border-r-brandLightOpacity10
+              //       ${
+              //         index + 1 === 1 || index + 1 === 2 || index + 1 === 3
+              //           ? "pt-0"
+              //           : "pt-5"
+              //       }
+              //       ${
+              //         index + 1 === 3 || index + 1 === 6
+              //           ? "border-0"
+              //           : `border-r`
+              //       }
+
+              //       `}
+              //     >
+              //       <div className="bookmark text-textColorGray hover:text-textColor  dark:text-brandLightOpacity70 dark:hover:text-brandLightOpacity100">
+              //         <i className="fa-regular fa-bookmark "></i>
+              //       </div>
+              //     </div>
+              //   </div>
+              // );
+            })}
+          </div>
+          <div className="flex flex-wrap pt-5 pb-5  bg-white rounded dark:bg-brandDark1 mw-lg:flex">
+            {items.slice(0, 4).map((elem, index) => {
+              return (
+                <div
+                  // p-5
+                  className={`w-1/3  mw-lg:w-1/2 mw-lg:w-full flex     ${
+                    style.news_items
+                  } border-grayCard  dark:border-brandLightOpacity10 ${
+                    index === 2
+                      ? "mw-12:border-r-grayCard mw-12:dark:border-r-brandLightOpacity10"
+                      : ""
+                  }  `}
+                  key={index}
+                >
                   <div
-                    className={`${
-                      style.thumnail_img
-                    }  pr-5  border-r-grayCard
-                    ${
-                      index + 1 === 1 || index + 1 === 2 || index + 1 === 3
-                        ? "pt-0"
-                        : "pt-5"
-                    } 
-                    ${
-                      index + 1 === 3 || index + 1 === 6
-                        ? "border-0"
-                        : `border-r`
-                    }`}
+                    className={`${style.news_img_wrap}  rounded-sm border-grayCard  mw-12:border-b-grayCard  dark:border-brandLightOpacity10 `}
                   >
-                    <div className="bookmark text-textColorGray hover:text-textColor">
+                    <Image
+                      src={elem.picture}
+                      alt="Latest new"
+                      className="rounded-sm"
+                    />
+                  </div>
+                  <div
+                    className={`${style.news_desc} border-grayCard dark:border-brandLightOpacity10`}
+                  >
+                    <h4 className="text-base leading-5 text-brandDark2 font-bold montserratfont dark:text-brandLightOpacity100">
+                      Bayes Esports appoints York Scheunemann as COO of reputed
+                      gaming...
+                    </h4>
+                    <div className="tags flex py-4 montserratfont">
+                      <div className="text-textColorGray bg-grayCard px-2 py-1.5 rounded mr-2 text-xs leading-4 font-medium hover:bg-grayCardHover cursor-pointer dark:bg-brandLightOpacity10 dark:hover:bg-brandLightOpacity30 dark:text-brandLightOpacity70">
+                        News
+                      </div>
+                      <div className="text-textColorGray bg-grayCard px-2 py-1.5 rounded text-xs leading-4 font-medium hover:bg-grayCardHover cursor-pointer dark:bg-brandLightOpacity10 dark:hover:bg-brandLightOpacity30 dark:text-brandLightOpacity70">
+                        Esports
+                      </div>
+                    </div>
+                    <small className="text-textColorGray dark:text-brandLightOpacity70 text-xs montserratfont">
+                      <span>
+                        <i>3 hrs ago</i>
+                      </span>
+                      <span
+                        className="border-l border-l-textColorGray pl-2 ml-2 border-textColorGray
+              dark:border-l-brandLightOpacity10
+              "
+                      >
+                        5 min read
+                      </span>
+                    </small>
+                  </div>
+                  <div className={`${style.thumnail_img} `}>
+                    <div className="bookmark text-textColorGray hover:text-textColor  dark:text-brandLightOpacity70 dark:hover:text-brandLightOpacity100">
                       <i className="fa-regular fa-bookmark "></i>
                     </div>
                   </div>
