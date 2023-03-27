e560c984000cd985e46f8db62a8720201e46a8ff
seositesoft17@gmail.com <rehmanali1477@gmail.com>
2023-03-27 12:01:44 +0500

    updated code

--- src/layouts/header/Header.tsx
+++ src/layouts/header/Header.tsx
@@ -196,6 +196,7 @@ const Header = () => {
           leaveFrom="opacity-100"
           leaveTo="opacity-0"
         >
+          
           <div className="fixed inset-0 bg-brandDark1 bg-opacity-80 transition-opacity" />
         </Transition.Child>
         <div className="fixed inset-0 z-10 overflow-y-auto">
