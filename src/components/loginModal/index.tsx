b6a5d78a87408fd5e578d9f4f36e03eef3eb7282
seositesoft17@gmail.com <rehmanali1477@gmail.com>
2023-03-28 11:00:57 +0500

    working on login popup

--- src/components/loginModal/index.tsx
+++ src/components/loginModal/index.tsx
@@ -1,9 +1,87 @@
-import React from 'react'
-
-const LoginModal = () => {
+import React, { Fragment, useEffect, useRef, useState } from "react";
+import { Dialog, Transition } from "@headlessui/react";
+import { ExclamationTriangleIcon } from "@heroicons/react/24/outline";
+import Image from "next/image";
+import Banner from "../../assets/images/login/banner.png";
+import Logo from "../../assets/images/login/logo.png";
+import style from "./login.module.css";
+const LoginModal = (props: any) => {
+  const cancelButtonRef = useRef(null);
   return (
-    <div>LoginModal</div>
-  )
-}
+    <Transition.Root show={props.open} as={Fragment}>
+      <Dialog
+        as="div"
+        className="relative z-10"
+        initialFocus={cancelButtonRef}
+        onClose={() => props.onClickOpenModal()}
+      >
+        <Transition.Child
+          as={Fragment}
+          enter="ease-out duration-300"
+          enterFrom="opacity-0"
+          enterTo="opacity-100"
+          leave="ease-in duration-200"
+          leaveFrom="opacity-100"
+          leaveTo="opacity-0"
+        >
+          <div className="fixed inset-0 bg-brandDark1 bg-opacity-80 transition-opacity" />
+        </Transition.Child>
+        <div className="fixed inset-0 z-10 overflow-y-auto">
+          <div className="flex min-h-full items-end justify-center p-4 text-center sm:items-center sm:p-0">
+            <Transition.Child
+              as={Fragment}
+              enter="ease-out duration-300"
+              enterFrom="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
+              enterTo="opacity-100 translate-y-0 sm:scale-100"
+              leave="ease-in duration-200"
+              leaveFrom="opacity-100 translate-y-0 sm:scale-100"
+              leaveTo="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
+            >
+              <Dialog.Panel className="relative transform overflow-hidden rounded-lg bg-white text-left shadow-xl transition-all sm:my-8 ">
+                <div className="bg-brandDark2">
+                  <div className={style.image_h}>
+                    <Image
+                      src={Banner}
+                      className="max-w-[100&] h-full"
+                      alt="banner logo"
+                    />
+                  </div>
+                </div>
 
-export default LoginModal
+                <div className="flex flex-col ">
+                  <div className="flex flex-col items-center content-center pl-40 pr-40 ">
+                    <Image src={Logo} alt="logo" />
+                    <p>Log In to Gossip.gg</p>
+                    <input
+                      className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                      id="username"
+                      type="text"
+                      placeholder="Username"
+                    />
+                    <input
+                      className="shadow appearance-none border border-red-500 rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline"
+                      id="password"
+                      type="password"
+                      placeholder="******************"
+                    />
+                    <div className="flex flex-row justify-between items-center ">
+                      <div>
+                        <input type="checkbox" />
+                        Remember me
+                      </div>
+                      <div>
+                        <a href="">Forgot password?</a>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </Dialog.Panel>
+            </Transition.Child>
+          </div>
+        </div>
+      </Dialog>
+    </Transition.Root>
+  );
+};
+
+export default LoginModal;
--- tailwind.config.js
+++ tailwind.config.js
@@ -60,7 +60,7 @@ module.exports = {
       },
       margin: {
         '88px': '88px',
-      },
+      }
     },
   },
   plugins: [require("@tailwindcss/typography")],
