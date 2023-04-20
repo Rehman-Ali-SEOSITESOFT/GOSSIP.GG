7786a78428fd32c554773bf25b1f99a999c17270
sssghulamrasool <seositesoft7@gmail.com>
2023-04-20 11:25:44 +0500

    home page slider for responsive

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -18,17 +18,36 @@ export default function Home() {
     setIsLoggedIn(user);
     // router.refresh();
   }, []);
-
+  const responsive = {
+    tablet: {
+      breakpoint: { max: 639, min: 360 },
+      items: 1,
+      paritialVisibilityGutter: 200,
+      arrows: false,
+    },
+    tablet2: {
+      breakpoint: { max: 510, min: 460 },
+      items: 1,
+      paritialVisibilityGutter: 150,
+      arrows: false,
+    },
+    mobile: {
+      breakpoint: { max: 460, min: 360 },
+      items: 1,
+      paritialVisibilityGutter: 60,
+      arrows: false,
+    },
+  };
   return (
     <section className="home-page">
       <div className="pb-11  pt-6 mw-12:pb-7 mw-md:pb-[20px] mw-md:pt-[12px] ">
         <Trending />
       </div>
       <HeroSection />
-      <EsportEvent />
+      <EsportEvent responsive={responsive}  />
       <LatestNews />
       {isLoggedIn && <ForYou />}
-      <LiveMatch />
+      <LiveMatch responsive={responsive} />
       {isLoggedIn && <EmailVerify />}
     </section>
   );
--- src/components/LiveMatches/index.tsx
+++ src/components/LiveMatches/index.tsx
@@ -10,8 +10,9 @@ import DarkLine from "../../assets/images/general/dark-line.png";
 import SectionSaprator from "../SecSaprator";
 import style from "./liveMatch.module.css";
 import { useTheme } from "next-themes";
+import Carousel from "react-multi-carousel";
 
-const LiveMatch = () => {
+const LiveMatch = (props: any) => {
   interface MatchList {
     match_title: string;
     team_one_title: string;
@@ -114,15 +115,15 @@ const LiveMatch = () => {
           arrows: false,
         },
       },
-      {
-        breakpoint: 500,
-        settings: {
-          slidesToShow: 1,
-          slidesToScroll: 1,
-          arrows: false,
-          centerMode: true,
-        },
-      },
+      // {
+      //   breakpoint: 639,
+      //   settings: {
+      //     slidesToShow: 1,
+      //     slidesToScroll: 1,
+      //     arrows: false,
+      //     centerMode: true,
+      //   },
+      // },
     ],
   };
   return (
@@ -131,88 +132,122 @@ const LiveMatch = () => {
         <div className="pt-[88px] mw-lg:pt-7 mw-sm:pt-[16px]">
           <SectionSaprator title="LIVE MATCHES" />
         </div>
-        <Slider
-          className={`live-matches-slider ${
-            isDarkTheme === "dark" ? " dark-arrow" : " light-arrow"
-          }`}
-          {...settings}
-        >
-          {matchData.map((item, index) => (
-            <div
-              className="border  dark:border-brandLightOpacity10 bg-white hover:border-brandDark1 hover:dark:border-brand hover:dark:shadow-[0_0_8px_0_#ed4e50] hover:shadow-[0_0_8px_0_#e5e5e5] m-1  dark:bg-brandDark1 border-inherit dark:border-brandDark1 border rounded"
-              key={index}
-            >
-              <div className="flex flex-row justify-between items-center content-center p-4">
-                <div className="flex items-center">
-                  <Image
-                    className="max-w-[100%] max-h-[100%]"
-                    src={isDarkTheme === "dark" ? LogoDark : Logo}
-                    alt="Live Match logo"
-                  />
-                  <p
-                    className={`${style.font_live_match_16} pl-3 text-brandDark2 dark:text-brandLightOpacity100 ${style.font_text_live_page} font-semibold`}
-                  >
-                    {item.match_title}
-                  </p>
+        <div className="block mw-sm:hidden">
+          <Slider
+            className={`live-matches-slider ${
+              isDarkTheme === "dark" ? " dark-arrow" : " light-arrow"
+            }`}
+            {...settings}
+          >
+            {matchData.map((item, index) => (
+              <div
+                className="border  dark:border-brandLightOpacity10 bg-white hover:border-brandDark1 hover:dark:border-brand hover:dark:shadow-[0_0_8px_0_#ed4e50] hover:shadow-[0_0_8px_0_#e5e5e5] m-1  dark:bg-brandDark1 border-inherit dark:border-brandDark1 border rounded"
+                key={index}
+              >
+                <div className="flex flex-row justify-between items-center content-center p-4">
+                  <div className="flex items-center">
+                    <Image
+                      className="max-w-[100%] max-h-[100%]"
+                      src={isDarkTheme === "dark" ? LogoDark : Logo}
+                      alt="Live Match logo"
+                    />
+                    <p
+                      className={`${style.font_live_match_16} pl-3 text-brandDark2 dark:text-brandLightOpacity100 ${style.font_text_live_page} font-semibold`}
+                    >
+                      {item.match_title}
+                    </p>
+                  </div>
+                  {item.match_status !== "LIVE" ? (
+                    <div className="bg-grayCard dark:bg-brandLightOpacity10 opacity-100 h-8 max-w-[25%]  text-[14px]  flex justify-center items-center rounded-sm min-w-fit">
+                      <p className="text-brandDark2 dark:text-brandLightOpacity100 opacity-70 montserratfont font-medium mw-sm3:text-[8px]  text-[14px]  px-2 ">
+                        {item.match_status}
+                      </p>
+                    </div>
+                  ) : (
+                    <div className="bg-errorIndicator h-8 w-14 flex justify-center items-center rounded-sm min-w-fit">
+                      <p
+                        className={`text-white montserratfont  text-[14px]  tracking-wider  mw-sm3:text-[8px]  `}
+                      >
+                        {item.match_status}
+                      </p>
+                    </div>
+                  )}
                 </div>
-                {item.match_status !== "LIVE" ? (
-                  <div className="bg-grayCard dark:bg-brandLightOpacity10 opacity-100 h-8 max-w-[25%]  text-[14px]  flex justify-center items-center rounded-sm min-w-fit">
-                    <p className="text-brandDark2 dark:text-brandLightOpacity100 opacity-70 montserratfont font-medium mw-sm3:text-[8px]  text-[14px]  px-2 ">
-                      {item.match_status}
-                    </p>
+                <div className="flex justify-center items-center mw-sm1:hidden">
+                  <Image
+                    src={isDarkTheme === "dark" ? DarkLine : Line}
+                    className="w-[90%]"
+                    alt="saprator line"
+                  />
+                </div>
+                <div className=" p-4 flex flex-row justify-between mw-sm1:hidden ">
+                  <div className="flex flex-row justify-between">
+                    <div>
+                      <Image src={TeamOne} alt="Team logo" />
+                    </div>
+                    <div className="flex flex-col justify-center items-center pl-3">
+                      <p
+                        className={`text-brandDark2 dark:text-brandLightOpacity100 ${style.font_text_live_page} font-bold ${style.font_live_match_18}`}
+                      >
+                        {item.team_one_title}
+                      </p>
+                      <p
+                        className={`text-brandDark2 dark:text-brandLightOpacity100 ${style.font_text_live_page} text-1xl ${style.font_live_match_22} font-extrabold`}
+                      >
+                        {item.team_one_score}
+                      </p>
+                    </div>
                   </div>
-                ) : (
-                  <div className="bg-errorIndicator h-8 w-14 flex justify-center items-center rounded-sm min-w-fit">
+                  <div className="  ">
                     <p
-                      className={`text-white montserratfont  text-[14px]  tracking-wider  mw-sm3:text-[8px]  `}
+                      className={`${style.font_live_match_16} font-text-live-pag text-brandDark2 dark:text-brandLightOpacity100 opacity-70`}
                     >
-                      {item.match_status}
+                      vs
                     </p>
                   </div>
-                )}
-              </div>
-              <div className="flex justify-center items-center mw-sm1:hidden">
-                <Image
-                  src={isDarkTheme === "dark" ? DarkLine : Line}
-                  className="w-[90%]"
-                  alt="saprator line"
-                />
-              </div>
-              <div className=" p-4 flex flex-row justify-between mw-sm1:hidden ">
-                <div className="flex flex-row justify-between">
-                  <div>
-                    <Image src={TeamOne} alt="Team logo" />
+                  <div className=" flex flex-row justify-between">
+                    <div>
+                      <Image src={TeamTwo} alt="Team logo" />
+                    </div>
+                    <div className="flex flex-col justify-center items-center  pl-3">
+                      <p
+                        className={`text-brandDark2 dark:text-brandLightOpacity100 ${style.font_text_live_page} font-bold ${style.font_live_match_18}`}
+                      >
+                        {item.team_two_title}
+                      </p>
+                      <p
+                        className={`text-brandDark2 dark:text-brandLightOpacity100 ${style.font_text_live_page} text-1xl ${style.font_live_match_22} font-extrabold`}
+                      >
+                        {item.team_two_score}
+                      </p>
+                    </div>
                   </div>
-                  <div className="flex flex-col justify-center items-center pl-3">
-                    <p
-                      className={`text-brandDark2 dark:text-brandLightOpacity100 ${style.font_text_live_page} font-bold ${style.font_live_match_18}`}
-                    >
-                      {item.team_one_title}
-                    </p>
+                </div>
+                <div className=" hidden p-4 flex flex-row justify-between mw-sm1:block border-t-inherit border-t dark:border-t-brandLightOpacity10">
+                  <div className="flex flex-row justify-between items-center">
+                    <div className="flex flex-row justify-center items-center">
+                      <Image src={TeamOne} alt="Team logo" />
+                      <p
+                        className={`text-brandDark2 dark:text-brandLightOpacity100 pl-3 ${style.font_text_live_page} font-bold ${style.font_live_match_18}`}
+                      >
+                        {item.team_one_title}
+                      </p>
+                    </div>
                     <p
                       className={`text-brandDark2 dark:text-brandLightOpacity100 ${style.font_text_live_page} text-1xl ${style.font_live_match_22} font-extrabold`}
                     >
                       {item.team_one_score}
                     </p>
                   </div>
-                </div>
-                <div className="  ">
-                  <p
-                    className={`${style.font_live_match_16} font-text-live-pag text-brandDark2 dark:text-brandLightOpacity100 opacity-70`}
-                  >
-                    vs
-                  </p>
-                </div>
-                <div className=" flex flex-row justify-between">
-                  <div>
-                    <Image src={TeamTwo} alt="Team logo" />
-                  </div>
-                  <div className="flex flex-col justify-center items-center  pl-3">
-                    <p
-                      className={`text-brandDark2 dark:text-brandLightOpacity100 ${style.font_text_live_page} font-bold ${style.font_live_match_18}`}
-                    >
-                      {item.team_two_title}
-                    </p>
+                  <div className="flex flex-row justify-between items-center mt-4">
+                    <div className="flex flex-row justify-center items-center">
+                      <Image src={TeamTwo} alt="Team logo" />
+                      <p
+                        className={`text-brandDark2 dark:text-brandLightOpacity100 pl-3 ${style.font_text_live_page} font-bold ${style.font_live_match_18}`}
+                      >
+                        {item.team_two_title}
+                      </p>
+                    </div>
                     <p
                       className={`text-brandDark2 dark:text-brandLightOpacity100 ${style.font_text_live_page} text-1xl ${style.font_live_match_22} font-extrabold`}
                     >
@@ -221,41 +256,137 @@ const LiveMatch = () => {
                   </div>
                 </div>
               </div>
-              <div className=" hidden p-4 flex flex-row justify-between mw-sm1:block border-t-inherit border-t dark:border-t-brandLightOpacity10">
-                <div className="flex flex-row justify-between items-center">
-                  <div className="flex flex-row justify-center items-center">
-                    <Image src={TeamOne} alt="Team logo" />
+            ))}
+          </Slider>
+        </div>
+        <div className="hidden mw-sm:block slider-arrows">
+          <Carousel
+            responsive={props.responsive}
+            className="valorant-slider"
+            ssr
+            partialVisbile
+            itemClass="image-item"
+          >
+            {matchData.map((item, index) => (
+              <div
+                className="border  dark:border-brandLightOpacity10 bg-white hover:border-brandDark1 hover:dark:border-brand hover:dark:shadow-[0_0_8px_0_#ed4e50] hover:shadow-[0_0_8px_0_#e5e5e5] m-1  dark:bg-brandDark1 border-inherit dark:border-brandDark1 border rounded"
+                key={index}
+              >
+                <div className="flex flex-row justify-between items-center content-center p-4">
+                  <div className="flex items-center">
+                    <Image
+                      className="max-w-[100%] max-h-[100%]"
+                      src={isDarkTheme === "dark" ? LogoDark : Logo}
+                      alt="Live Match logo"
+                    />
                     <p
-                      className={`text-brandDark2 dark:text-brandLightOpacity100 pl-3 ${style.font_text_live_page} font-bold ${style.font_live_match_18}`}
+                      className={`${style.font_live_match_16} pl-3 text-brandDark2 dark:text-brandLightOpacity100 ${style.font_text_live_page} font-semibold`}
                     >
-                      {item.team_one_title}
+                      {item.match_title}
                     </p>
                   </div>
-                  <p
-                    className={`text-brandDark2 dark:text-brandLightOpacity100 ${style.font_text_live_page} text-1xl ${style.font_live_match_22} font-extrabold`}
-                  >
-                    {item.team_one_score}
-                  </p>
+                  {item.match_status !== "LIVE" ? (
+                    <div className="bg-grayCard dark:bg-brandLightOpacity10 opacity-100 h-8 max-w-[25%]  text-[14px]  flex justify-center items-center rounded-sm min-w-fit">
+                      <p className="text-brandDark2 dark:text-brandLightOpacity100 opacity-70 montserratfont font-medium mw-sm3:text-[8px]  text-[14px]  px-2 ">
+                        {item.match_status}
+                      </p>
+                    </div>
+                  ) : (
+                    <div className="bg-errorIndicator h-8 w-14 flex justify-center items-center rounded-sm min-w-fit">
+                      <p
+                        className={`text-white montserratfont  text-[14px]  tracking-wider  mw-sm3:text-[8px]  `}
+                      >
+                        {item.match_status}
+                      </p>
+                    </div>
+                  )}
                 </div>
-                <div className="flex flex-row justify-between items-center mt-4">
-                  <div className="flex flex-row justify-center items-center">
-                    <Image src={TeamTwo} alt="Team logo" />
+                <div className="flex justify-center items-center mw-sm1:hidden">
+                  <Image
+                    src={isDarkTheme === "dark" ? DarkLine : Line}
+                    className="w-[90%]"
+                    alt="saprator line"
+                  />
+                </div>
+                <div className=" p-4 flex flex-row justify-between mw-sm1:hidden ">
+                  <div className="flex flex-row justify-between">
+                    <div>
+                      <Image src={TeamOne} alt="Team logo" />
+                    </div>
+                    <div className="flex flex-col justify-center items-center pl-3">
+                      <p
+                        className={`text-brandDark2 dark:text-brandLightOpacity100 ${style.font_text_live_page} font-bold ${style.font_live_match_18}`}
+                      >
+                        {item.team_one_title}
+                      </p>
+                      <p
+                        className={`text-brandDark2 dark:text-brandLightOpacity100 ${style.font_text_live_page} text-1xl ${style.font_live_match_22} font-extrabold`}
+                      >
+                        {item.team_one_score}
+                      </p>
+                    </div>
+                  </div>
+                  <div className="  ">
                     <p
-                      className={`text-brandDark2 dark:text-brandLightOpacity100 pl-3 ${style.font_text_live_page} font-bold ${style.font_live_match_18}`}
+                      className={`${style.font_live_match_16} font-text-live-pag text-brandDark2 dark:text-brandLightOpacity100 opacity-70`}
                     >
-                      {item.team_two_title}
+                      vs
                     </p>
                   </div>
-                  <p
-                    className={`text-brandDark2 dark:text-brandLightOpacity100 ${style.font_text_live_page} text-1xl ${style.font_live_match_22} font-extrabold`}
-                  >
-                    {item.team_two_score}
-                  </p>
+                  <div className=" flex flex-row justify-between">
+                    <div>
+                      <Image src={TeamTwo} alt="Team logo" />
+                    </div>
+                    <div className="flex flex-col justify-center items-center  pl-3">
+                      <p
+                        className={`text-brandDark2 dark:text-brandLightOpacity100 ${style.font_text_live_page} font-bold ${style.font_live_match_18}`}
+                      >
+                        {item.team_two_title}
+                      </p>
+                      <p
+                        className={`text-brandDark2 dark:text-brandLightOpacity100 ${style.font_text_live_page} text-1xl ${style.font_live_match_22} font-extrabold`}
+                      >
+                        {item.team_two_score}
+                      </p>
+                    </div>
+                  </div>
+                </div>
+                <div className=" hidden p-4 flex flex-row justify-between mw-sm1:block border-t-inherit border-t dark:border-t-brandLightOpacity10">
+                  <div className="flex flex-row justify-between items-center">
+                    <div className="flex flex-row justify-center items-center">
+                      <Image src={TeamOne} alt="Team logo" />
+                      <p
+                        className={`text-brandDark2 dark:text-brandLightOpacity100 pl-3 ${style.font_text_live_page} font-bold ${style.font_live_match_18}`}
+                      >
+                        {item.team_one_title}
+                      </p>
+                    </div>
+                    <p
+                      className={`text-brandDark2 dark:text-brandLightOpacity100 ${style.font_text_live_page} text-1xl ${style.font_live_match_22} font-extrabold`}
+                    >
+                      {item.team_one_score}
+                    </p>
+                  </div>
+                  <div className="flex flex-row justify-between items-center mt-4">
+                    <div className="flex flex-row justify-center items-center">
+                      <Image src={TeamTwo} alt="Team logo" />
+                      <p
+                        className={`text-brandDark2 dark:text-brandLightOpacity100 pl-3 ${style.font_text_live_page} font-bold ${style.font_live_match_18}`}
+                      >
+                        {item.team_two_title}
+                      </p>
+                    </div>
+                    <p
+                      className={`text-brandDark2 dark:text-brandLightOpacity100 ${style.font_text_live_page} text-1xl ${style.font_live_match_22} font-extrabold`}
+                    >
+                      {item.team_two_score}
+                    </p>
+                  </div>
                 </div>
               </div>
-            </div>
-          ))}
-        </Slider>
+            ))}
+          </Carousel>
+        </div>
       </div>
     </section>
   );
