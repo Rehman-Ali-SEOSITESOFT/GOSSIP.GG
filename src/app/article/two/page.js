06d8bd0afe861d80292567f376ea933159c17421
sssghulamrasool <seositesoft7@gmail.com>
2023-04-27 19:38:14 +0500

    article props and cons and readmore like

--- src/app/article/two/page.js
+++ src/app/article/two/page.js
@@ -1,10 +1,12 @@
 import ArticlePropConsBox from "@/components/Article/ArticlePropConsBox";
+import ReadMoreLikeThis from "@/components/Article/ReadMore";
 import React from "react";
 
 const Page = () => {
   return (
     <div>
       <ArticlePropConsBox />
+      <ReadMoreLikeThis />
     </div>
   );
 };
