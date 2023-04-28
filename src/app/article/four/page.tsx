ab81242bbd133a34ee6c8df3d3f2e7f3f236f323
seositesoft17@gmail.com <rehmanali1477@gmail.com>
2023-04-28 14:39:15 +0500

    working on slider

--- src/app/article/four/page.tsx
+++ src/app/article/four/page.tsx
@@ -5,19 +5,20 @@ import ListBox from '@/components/Article/Listing';
 import Comments from '@/components/Article/Comments';
 import React from 'react'
 import UserLogoSection from '@/components/Article/UserInfoSection';
+import AsNavFor from '@/components/Article/Slider';
 
 const Page = () => {
   return (
     <div>
-      <ListBox/>
+      {/* <ListBox/>
        <div className='mt-[48px]'>
       <UserLogoSection/>
       </div>
       <div className='mt-[10px]'>
       <Comments/>
      
-      </div>
-      
+      </div> */}
+        <AsNavFor/>      
     </div>
   )
 }
