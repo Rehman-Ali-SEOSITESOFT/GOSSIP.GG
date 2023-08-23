c3363fc004befb2b7066985068fd8bb1d640d3c6
sssghulamrasool <seositesoft7@gmail.com>
2023-08-23 12:38:01 +0500

    complete add screen

--- src/components/QuizRatio/index.tsx
+++ src/components/QuizRatio/index.tsx
@@ -100,15 +100,15 @@ const QuizRatio = () => {
       </div>
 
       {/* <IntroScreen open={openIntro} close={hanldeOpenedQuiz} /> */}
-      {openIntro && <AddScreen open={openIntro} close={hanldeOpenedQuiz} />}
+      {/* {openIntro && <AddScreen open={openIntro} close={hanldeOpenedQuiz} />} */}
       {/* <QuestionScreen open={openIntro} close={hanldeOpenedQuiz} /> */}
       {/* <AnswerSubmitScreen open={openIntro} close={hanldeOpenedQuiz} /> */}
       {/* <TermsAndCondition open={openIntro} close={hanldeOpenedQuiz} /> */}
-      {/* <WinnerScreenTwo open={openIntro} close={hanldeOpenedQuiz} /> */}
+      <WinnerScreenTwo open={openIntro} close={hanldeOpenedQuiz} />
       {/* <QuizStartTiemScreen open={openIntro} close={hanldeOpenedQuiz} /> */}
       {/* <AnsweredIncorrectly open={openIntro} close={hanldeOpenedQuiz} /> */}
       {/* <AnsweredCorrectly open={openIntro} close={hanldeOpenedQuiz} /> */}
-      <PriceOfTheDay open={openIntro} close={hanldeOpenedQuiz} />
+      {/* <PriceOfTheDay open={openIntro} close={hanldeOpenedQuiz} /> */}
     </>
   );
 };
