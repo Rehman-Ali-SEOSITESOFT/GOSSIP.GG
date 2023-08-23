83ed2eb5a0ddcd0f281c018f154068f5dacbb2c0
SSSAwais <seositesoft5@gmail.com>
2023-08-23 18:23:29 +0500

    rewards pages

--- src/components/Rewards/Faqs/FaqsAccordian/FaqsAccordian.tsx
+++ src/components/Rewards/Faqs/FaqsAccordian/FaqsAccordian.tsx
@@ -9,19 +9,19 @@ const FaqsAccordian = (props: any) => {
           <Disclosure>
             {({ open }) => (
               <>
-                <Disclosure.Button className="flex w-full justify-between rounded-lg pt-[29px] pb-[16px] text-left ">
-                  <p className="montserratfont text-[18px] font-semibold leading-normal text-[#fff] ">
+                <Disclosure.Button className="flex w-full justify-between rounded-lg pt-[8px] pb-[16px] text-left items-center">
+                  <p className="montserratfont text-[18px] font-mediu m leading-normal text-[#E5E5E5] mw-lg:text-[16px] mw-md:text-[14px]">
                     {props.heading}
                   </p>
                   <i
-                    className={`fa-solid fa-angle-up text-[#fff] text-[16px] ${
+                    className={`fa-solid fa-angle-up text-[#E5E5E5] text-[16px] ${
                       open ? "rotate-180 transform" : null
                     }`}
                   ></i>
                 </Disclosure.Button>
                 <Disclosure.Panel className="text-sm text-[#fff] pb-[15px]">
-                  If you&#39;re unhappy with your purchase for any reason, email us
-                  within 90 days and we&#39;ll refund you in full, no questions
+                  If you're unhappy with your purchase for any reason, email us
+                  within 90 days and we'll refund you in full, no questions
                   asked.
                 </Disclosure.Panel>
               </>
