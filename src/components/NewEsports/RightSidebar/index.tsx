39062b7848d99c928bbb7f44a4f05110ba2063dd
SSSAwais <seositesoft5@gmail.com>
2023-10-16 10:47:30 +0500

    issues

--- src/components/NewEsports/RightSidebar/index.tsx
+++ src/components/NewEsports/RightSidebar/index.tsx
@@ -5,10 +5,12 @@ import Image from "next/image";
 const Advertise = () => {
   return (
     <>
-      <aside className="sticky top-[150px] w-[200px] mw-md:hidden">
-        <Image src={ad1} alt="top " className="ml-auto mr-0  mb-[12px]" />
-        <Image src={ad2} alt="bottom " className="ml-auto mr-0" />
-      </aside>
+      <div className="w-[200px] mw-md:hidden">
+        <aside className="sticky top-[150px]">
+          <Image src={ad1} alt="top " className="ml-auto mr-0  mb-[12px]" />
+          <Image src={ad2} alt="bottom " className="ml-auto mr-0" />
+        </aside>
+      </div>
     </>
   );
 };
